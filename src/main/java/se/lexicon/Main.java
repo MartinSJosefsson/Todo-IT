52a0988942ea5c0d7515ca85133eaed3017d3f18
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-04-07 12:52:08 +0200

    Initial commit Todo 1

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,40 @@
 package se.lexicon;
 
+import org.junit.Test;
+import java.time.LocalDate;
+import static org.junit.Assert.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Todo IT");
+        class PersonTest {
+
+            @Test
+            public void testGetSummary() {
+                Person person = new Person(1, "Martin", "Josefsson", "martin.josefsson@test.test");
+                assertEquals("{id: 1, name: Martin Josefsson, email: martin.josefsson@test.test}", person.getSummary());
+            }
+        }
+
+        /*class TodoItemTest {
+
+            @Test
+            public void testIsOverdue() {
+                Person creator = new Person(1, "Martin", "Josefsson", "martin.josefsson@test.test");
+                TodoItem todoItem = new TodoItem(1, "Task 1", "Description", LocalDate.now().minusDays(1), creator);
+                assertTrue(todoItem.isOverdue());
+            }
+        }*/
+
+        /*class TodoItemTaskTest {
+
+            @Test
+            public void testGetSummary() {
+                Person assignee = new Person(1, "Martin", "Josefsson", "martin.josefsson@test.test");
+                TodoItem todoItem = new TodoItem(1, "Task 1", "Description", LocalDate.now().plusDays(1), assignee);
+                TodoItemTask task = new TodoItemTask(1, todoItem, assignee);
+                assertEquals("{id: 1, task: Task 1, assigned: true}", task.getSummary());
+            }
+        }*/
+
     }
 }
