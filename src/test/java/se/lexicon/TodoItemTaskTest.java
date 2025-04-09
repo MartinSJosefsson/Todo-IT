27f29906e34a46d3f0942c1f856236c17d311376
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-04-09 14:56:30 +0200

    Junit5

--- src/test/java/se/lexicon/TodoItemTaskTest.java
+++ src/test/java/se/lexicon/TodoItemTaskTest.java
@@ -1,10 +1,11 @@
 package se.lexicon;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 public class TodoItemTaskTest {
     @Test
--- src/test/java/se/lexicon/TodoItemTest.java
+++ src/test/java/se/lexicon/TodoItemTest.java
@@ -1,10 +1,12 @@
 package se.lexicon;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class TodoItemTest {
     @Test
