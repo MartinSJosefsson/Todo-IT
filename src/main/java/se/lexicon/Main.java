5bc1dd93662a03675177ba10b2c7bbe1c65433e6
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-04-22 07:18:19 +0200

    Junit5

--- src/main/java/se/lexicon/Main.java
+++ src/main/java/se/lexicon/Main.java
@@ -1,7 +1,11 @@
 package se.lexicon;
 
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Todo IT");
+        {
+
+        }
     }
 }
