1390133891c4490683cc2ceda92847bea289f421
MartinSJosefsson <martin.s.josefsson@gmail.com>
2025-04-22 07:21:16 +0200

    Junit5

--- src/main/java/se/lexicon/TodoItemTask.java
+++ src/main/java/se/lexicon/TodoItemTask.java
@@ -1,4 +1,46 @@
 package se.lexicon;
 
+import java.util.Objects;
+
 public class TodoItemTask {
+    private int id;
+    private boolean assigned;
+    private TodoItem todoItem;
+
+    public TodoItemTask(int id, boolean assigned, TodoItem todoItem) {
+        this.id = id;
+        this.assigned = assigned;
+        this.todoItem = todoItem;
+    }
+
+
+    public int getId() { return id; }
+    public boolean isAssigned() { return assigned; }
+    public TodoItem getTodoItem() { return todoItem; }
+
+    public void setAssigned(boolean assigned) { this.assigned = assigned; }
+
+    @Override
+    public String toString() {
+        return "TodoItemTask{" +
+                "id=" + id +
+                ", assigned=" + assigned +
+                ", todoItem=" + todoItem +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TodoItemTask)) return false;
+        TodoItemTask that = (TodoItemTask) o;
+        return id == that.id &&
+                assigned == that.assigned &&
+                Objects.equals(todoItem, that.todoItem);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, assigned, todoItem);
+    }
 }
